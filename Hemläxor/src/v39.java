7db49180e1d7352080e6824c3513d069e2f543d9
harisssssss <haris.dervisevic@elev.ga.ntig.se>
2019-09-26 10:52:13 +0200

    r2 kattis uppgift

---
+++ Hemläxor/src/v39.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class v39 {
+
+	public static void main(String[] args) {
+		
+	
+ Scanner input=new Scanner(System.in);
+ 
+ System.out.println("Write down two nummbers");
+ int s = input.nextInt();
+ int R1 = input.nextInt();
+ 
+int R2=2*s -R1;
+ 
+ System.out.println(R2);
+ 
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
