229abfb99592628457d7437ccbc7c6ada602406b
harisssssss <haris.dervisevic@elev.ga.ntig.se>
2019-09-30 11:16:32 +0200

    Create hemläxa.java

---
+++ Hemläxor/src/hemläxa.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class hemläxa {
+	
+	static Scanner input=new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+	int varv = input.nextInt();
+		
+	for(int a = 1; a <= varv; a++) {
+		System.out.println(a +"Abracadabra");
+	}
+		
+
+	}
+	
+
+}
