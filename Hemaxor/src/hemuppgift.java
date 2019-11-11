a41f6d56a791483e6f004e00593d4bab109cd905
harisssssss <haris.dervisevic@elev.ga.ntig.se>
2019-11-11 11:05:22 +0100

    Första method

---
+++ Hemaxor/src/hemuppgift.java
@@ -0,0 +1,22 @@
+
+public class hemuppgift {
+	static double g=9.82;
+	public static void main(String[] args) {
+		
+		System.out.println(fahrenheitTocelsius(50.));
+		
+	
+		
+		
+	}
+	
+	public static double fahrenheitTocelsius(double fahrenheit) {
+		return (fahrenheit-32)/1.8;
+	}
+	
+
+	
+	
+	
+	
+}
